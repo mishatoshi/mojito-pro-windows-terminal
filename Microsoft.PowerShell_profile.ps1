7fa1f2bca814bdca8f637d01288b51e4722654c1
Mikhail Sharapov <mishatoshi@gmail.com>
2024-11-16 19:04:48 +0300

    fixed: color substitution in Microsoft.PowerShell_profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -11,17 +11,17 @@ $PSReadLineOptions = @{
         "Operator" = "#7ECE7E"
         "Type" = "#FF764D"
         "Variable" = "#FFB74D"
-        "Parameter" = "#1F7A4D"
-        "ContinuationPrompt" = "#D6F5E6"
-        "Comment" = "#1F7A4D"
-        "Default" = "#D6F5E6"
+        "Parameter" = "#25744D"
+        "ContinuationPrompt" = "#D8F3E6"
+        "Comment" = "#25744D"
+        "Default" = "#D8F3E6"
         "Error" = "#DA190B"
         "Keyword" = "#7ECE7E"
         "Selection" = "#0D8BF2"
         "ListPrediction" = "#7ECE7E"
-        "InlinePrediction" = "#1F7A4D"
+        "InlinePrediction" = "#25744D"
         "ListPredictionSelected" = "#B4C41C"
-        "ListPredictionTooltip" = "#1F7A4D"
+        "ListPredictionTooltip" = "#25744D"
     }
 }
 Set-PSReadLineOption @PSReadLineOptions
